2622e75e55bcb3551e60e1df82ab79138cb9874a
Chris McGrath <chris@chrismcg.com>
2014-10-26 18:09:16 +0000

    Add SimpleCache.Store

--- lib/simple_cache.ex
+++ lib/simple_cache.ex
@@ -4,6 +4,7 @@ defmodule SimpleCache do
   # See http://elixir-lang.org/docs/stable/elixir/Application.html
   # for more information on OTP Applications
   def start(_type, _args) do
+    SimpleCache.Store.init
     SimpleCache.Supervisor.start_link
   end
 end
