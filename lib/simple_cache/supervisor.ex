e1f40dac7e1cf22aedc029888922ba07619d591d
Chris McGrath <chris@chrismcg.com>
2014-10-26 17:34:04 +0000

    Move the Supervisor out into it's own module
    Matches what's in the book better for now.

---
+++ lib/simple_cache/supervisor.ex
@@ -0,0 +1,28 @@
+defmodule SimpleCache.Supervisor do
+  use Supervisor
+
+  @server __MODULE__
+
+  def start_link do
+    Supervisor.start_link __MODULE__, [], name: @server
+  end
+
+  def start_child(value, lease_time) do
+    Supervisor.start_child __MODULE__, [value, lease_time]
+  end
+
+  def init([]) do
+    children = [
+      worker(Element, restart: :temporary, shutdown: :brutal_kill)
+    ]
+
+    # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [
+      strategy: :simple_one_for_one,
+      max_restarts: 0,
+      max_seconds: 1
+    ]
+    supervise(children, opts)
+  end
+end
